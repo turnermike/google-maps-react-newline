df5bb5027a90e3c63e5c4684916a49988bbe1999
Mike Turner <turner.mike@gmail.com>
2020-02-25 14:30:39 -0500

    basic marker example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,308 +6,346 @@ import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const keys = require('./config/keys');
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-}
+// const mapStyles = {
+//   width: '100%',
+//   height: '100%'
+// }
 
 
-const camelize = function(str) {
+// const camelize = function(str) {
 
-  return str.split(' ').map(function(word){
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }).join('');
+//   return str.split(' ').map(function(word){
+//     return word.charAt(0).toUpperCase() + word.slice(1);
+//   }).join('');
 
-}
+// }
 
-const evtNames = ['ready', 'click', 'dragend'];
-
-
-
-export class MapContainer extends React.Component {
+// const evtNames = ['ready', 'click', 'dragend'];
 
 
 
-
-
-  static defaultProps = {
-
-    zoom: 10,
-    initialCenter: {
-      lat: 37.774929,
-      lng: -122.419416
-    },
-    centerAroundCurrentLocation: true,
-    onMove: function() {}
-
-  }
+// export class MapContainer extends React.Component {
 
 
 
 
 
-  constructor(props) {
+//   static defaultProps = {
 
-    super(props);
+//     zoom: 10,
+//     initialCenter: {
+//       lat: 37.774929,
+//       lng: -122.419416
+//     },
+//     centerAroundCurrentLocation: true,
+//     onMove: function() {}
 
-    const {lat, lng} = this.props.initialCenter;
-
-    this.state = {
-      currentLocation: {
-        lat: lat,
-        lng: lng
-      }
-    }
-
-    // this.state = {
-    //   stores: [
-    //     { lat: 43.394348, lng: -79.822962 },    // my house
-    //     { lat: 43.519860, lng: -79.862151 },    // cuddly corner
-    //     { lat: 43.521502, lng: -79.865997 }     // e.w. foster
-    //   ]
-    // };
-
-  };
+//   }
 
 
 
 
 
-  componentDidUpdate(prevProps, prevState) {
+//   constructor(props) {
 
-    console.log('componentDidUpdate()');
+//     super(props);
 
-    if( prevProps.google !== this.props.google ) {
-      this.loadMap();
-    }
+//     const {lat, lng} = this.props.initialCenter;
 
-    if( prevState.currentLocation !== this.state.currentLocation ) {
-      this.recenterMap();
-    }
+//     this.state = {
+//       currentLocation: {
+//         lat: lat,
+//         lng: lng
+//       }
+//     }
 
-  };
+//     // this.state = {
+//     //   stores: [
+//     //     { lat: 43.394348, lng: -79.822962 },    // my house
+//     //     { lat: 43.519860, lng: -79.862151 },    // cuddly corner
+//     //     { lat: 43.521502, lng: -79.865997 }     // e.w. foster
+//     //   ]
+//     // };
 
-
-
-
-
-  componentDidMount() {
-
-    console.log('componentDidMount()');
-
-    if( this.props.centerAroundCurrentLocation ) {
-
-      if( navigator && navigator.geolocation ) {
-
-        navigator.geolocation.getCurrentPosition( (pos) => {
-          const coords = pos.coords;
-          this.setState({
-            currentLocation: {
-              lat: coords.latitude,
-              lng: coords.longitude
-            }
-          })
-        })
-
-      }
-
-    }
-
-    this.loadMap();
-
-  };
+//   };
 
 
 
 
 
-  loadMap() {
+//   componentDidUpdate(prevProps, prevState) {
 
-    console.log('loadMap()');
+//     console.log('componentDidUpdate()');
 
-    if( this.props && this.props.google ) {
+//     if( prevProps.google !== this.props.google ) {
+//       this.loadMap();
+//     }
 
-      // google is available
-      const {google} = this.props;
-      const maps = google.maps;
-      // console.log('maps', maps);
+//     if( prevState.currentLocation !== this.state.currentLocation ) {
+//       this.recenterMap();
+//     }
 
-      const mapRef = this.refs.map;
-      // console.log('mapRef', mapRef);
-      const mapDOMNode = ReactDOM.findDOMNode(mapRef);      // reference to actual DOM node, not the virtual DOM
-      // console.log('mapDOMNode', mapDOMNode);
+//   };
 
-      let {initialCenter, zoom} = this.props;
-      // const {lat, lng} = initialCenter;                     // set lat, lng using props
-      const {lat, lng} = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
-      const mapConfig = Object.assign({}, {
-        center: center,
-        zoom: zoom
-      })
-      this.map = new maps.Map(mapDOMNode, mapConfig);
 
-      // // basic event listener
-      // this.map.addListener('dragend', (evt) => {
 
-      //   this.props.onMove(this.map);
-      //   console.log('moved');
+
+
+//   componentDidMount() {
+
+//     console.log('componentDidMount()');
+
+//     if( this.props.centerAroundCurrentLocation ) {
+
+//       if( navigator && navigator.geolocation ) {
+
+//         navigator.geolocation.getCurrentPosition( (pos) => {
+//           const coords = pos.coords;
+//           this.setState({
+//             currentLocation: {
+//               lat: coords.latitude,
+//               lng: coords.longitude
+//             }
+//           })
+//         })
+
+//       }
+
+//     }
+
+//     this.loadMap();
+
+//   };
+
+
+
+
+
+//   loadMap() {
+
+//     console.log('loadMap()');
+
+//     if( this.props && this.props.google ) {
+
+//       // google is available
+//       const {google} = this.props;
+//       const maps = google.maps;
+//       // console.log('maps', maps);
+
+//       const mapRef = this.refs.map;
+//       // console.log('mapRef', mapRef);
+//       const mapDOMNode = ReactDOM.findDOMNode(mapRef);      // reference to actual DOM node, not the virtual DOM
+//       // console.log('mapDOMNode', mapDOMNode);
+
+//       let {initialCenter, zoom} = this.props;
+//       // const {lat, lng} = initialCenter;                     // set lat, lng using props
+//       const {lat, lng} = this.state.currentLocation;
+//       const center = new maps.LatLng(lat, lng);
+//       const mapConfig = Object.assign({}, {
+//         center: center,
+//         zoom: zoom
+//       })
+//       this.map = new maps.Map(mapDOMNode, mapConfig);
+
+//       // // basic event listener
+//       // this.map.addListener('dragend', (evt) => {
+
+//       //   this.props.onMove(this.map);
+//       //   console.log('moved');
         
-      // });
+//       // });
 
-      // // event listener with timeout to prevent multiple events
-      // let centerChangedTimeout;
-      // this.map.addListener('dragend', (evt) => {
+//       // // event listener with timeout to prevent multiple events
+//       // let centerChangedTimeout;
+//       // this.map.addListener('dragend', (evt) => {
 
-      //   if( centerChangedTimeout ) {
-      //     clearTimeout(centerChangedTimeout);
-      //     centerChangedTimeout = null;
-      //   }
+//       //   if( centerChangedTimeout ) {
+//       //     clearTimeout(centerChangedTimeout);
+//       //     centerChangedTimeout = null;
+//       //   }
 
-      //   centerChangedTimeout = setTimeout(() => {
-      //     this.props.onMove(this.map);
-      //     console.log('moved');
-      //   }, 0);
+//       //   centerChangedTimeout = setTimeout(() => {
+//       //     this.props.onMove(this.map);
+//       //     console.log('moved');
+//       //   }, 0);
         
-      // });
+//       // });
 
-      evtNames.forEach(e => {
-        // console.log('event - ', e);
-        this.map.addListener(e, this.handleEvent(e));
-      });
+//       evtNames.forEach(e => {
+//         // console.log('event - ', e);
+//         this.map.addListener(e, this.handleEvent(e));
+//       });
 
-      maps.event.trigger(this.map, 'ready');
-
-
-
-
-    }
-
-  };
+//       maps.event.trigger(this.map, 'ready');
 
 
 
 
+//     }
 
-
-  recenterMap() {
-
-    const map = this.map;
-    const curr = this.state.currentLocation;
-    const google = this.props.google;
-    const maps = google.maps;
-
-    if( map ) {
-
-      let center = new maps.LatLng( curr.lat, curr.lng );
-      map.panTo( center );
-
-    }
-
-  };
+//   };
 
 
 
 
 
 
-  handleEvent(evtName) {
+//   recenterMap() {
 
-    // console.log('handleEvent()', evtName);
+//     const map = this.map;
+//     const curr = this.state.currentLocation;
+//     const google = this.props.google;
+//     const maps = google.maps;
 
-    let timeout;
-    // const handlerName = evtName;
-    const handlerName  = `on${camelize(evtName)}`;
-    console.log('handlerName', handlerName);
+//     if( map ) {
 
-    return (e) => {
+//       let center = new maps.LatLng( curr.lat, curr.lng );
+//       map.panTo( center );
 
-      if( timeout ) {
-        clearTimeout( timeout );
-        timeout = null;
-      }
+//     }
 
-      timeout = setTimeout(() => {
+//   };
+
+
+
+
+
+
+//   handleEvent(evtName) {
+
+//     // console.log('handleEvent()', evtName);
+
+//     let timeout;
+//     // const handlerName = evtName;
+//     const handlerName  = `on${camelize(evtName)}`;
+//     console.log('handlerName', handlerName);
+
+//     return (e) => {
+
+//       if( timeout ) {
+//         clearTimeout( timeout );
+//         timeout = null;
+//       }
+
+//       timeout = setTimeout(() => {
         
-        console.log('handleEvent timeout: ', handlerName);
+//         console.log('handleEvent timeout: ', handlerName);
 
-        if( this.props[handlerName] ) {
+//         if( this.props[handlerName] ) {
           
-          this.props[handlerName](this.props, this.map, e);
-        }
+//           this.props[handlerName](this.props, this.map, e);
+//         }
 
-      }, 0);
-
-
-
-    }
-
-  };
+//       }, 0);
 
 
+
+//     }
+
+//   };
 
 
 
 
-  displayMarkers = () => {
-
-    return this.state.stores.map( (store, index) => {
-
-      return <Marker key={index} id={index} position={{
-        lat: store.lat,
-        lng: store.lng
-      }}
-      onClick={ () => console.log('clicked me!') }
-      />
-
-    });
-
-  };
 
 
+//   displayMarkers = () => {
+
+//     return this.state.stores.map( (store, index) => {
+
+//       return <Marker key={index} id={index} position={{
+//         lat: store.lat,
+//         lng: store.lng
+//       }}
+//       onClick={ () => console.log('clicked me!') }
+//       />
+
+//     });
+
+//   };
 
 
+
+
+
+//   render() {
+
+//     return (
+//       <div className="position-static" ref='map'>
+//         Loading map...
+//       </div>
+//     )
+
+//   } 
+
+//   // render() {
+
+//   //   return (
+
+//   //     <Map
+//   //       google={this.props.google}
+//   //       zoom={10}
+//   //       style={mapStyles}
+//   //       initialCenter={{ lat: 43.3812341, lng: -79.9099591 }}
+//   //     >
+//   //       { this.displayMarkers() }
+//   //     </Map>
+
+//   //   );
+
+//   // }
+
+// }
+
+// Map.propTypes = {
+//   google: PropTypes.object,
+//   zoom: PropTypes.number,
+//   initialCenter: PropTypes.object,
+//   centerAroundCurrentLocation: PropTypes.bool,
+//   onMove: PropTypes.func
+// }
+
+// evtNames.forEach(e => (Map.propTypes[camelize(e)] = PropTypes.func));
+
+
+
+
+export class Container extends React.Component {
 
   render() {
 
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    }
+
+    const initialPos = {lat: 43.394348, lng: -79.822962}
+    const beerStorePos = { lat: 43.395958, lng: -79.793417}
+
     return (
-      <div className="position-static" ref='map'>
-        Loading map...
+
+      <div style={style}>
+        <Map 
+          google={this.props.google}
+          zoom={10}
+          initialCenter={initialPos}
+        >
+          {/*initialCenter marker*/}
+          <Marker /> 
+          <Marker position={beerStorePos} />
+        </Map>
       </div>
-    )
 
-  } 
+    );
 
-  // render() {
+  };
 
-  //   return (
+} // Container Component
 
-  //     <Map
-  //       google={this.props.google}
-  //       zoom={10}
-  //       style={mapStyles}
-  //       initialCenter={{ lat: 43.3812341, lng: -79.9099591 }}
-  //     >
-  //       { this.displayMarkers() }
-  //     </Map>
 
-  //   );
 
-  // }
-
-}
-
-Map.propTypes = {
-  google: PropTypes.object,
-  zoom: PropTypes.number,
-  initialCenter: PropTypes.object,
-  centerAroundCurrentLocation: PropTypes.bool,
-  onMove: PropTypes.func
-}
-
-evtNames.forEach(e => (Map.propTypes[camelize(e)] = PropTypes.func));
 
 export default GoogleApiWrapper({
   apiKey: keys.googleAPIKey
-})(MapContainer);
+})(Container);
 
